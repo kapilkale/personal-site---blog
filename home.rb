0a031d959cbb7f0b58154c3acd52c0a2a551035c
Kapil <kapil.kale.07@gmail.com>
2012-09-28 19:35:03 -0700

    jekyll now fully operational on site

--- home.rb
+++ home.rb
@@ -1,14 +1,16 @@
 require 'sinatra'
 require 'haml'
 
+# Blog stuff.
+get '/blog/?' do
+  File.read(File.join('public/blog/index.html'))
+end
+
 get '/blog/:post/?' do
   File.read(File.join('public/blog', "#{params[:post]}", "index.html"))
 end
 
+# Homepage.
 get '/' do
   haml :home
-end
-
-get '/blog/?' do
-  haml :blog
 end
---
+++ jekyll/_posts/2009-08-07-first-post.markdown
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "First post"
+date_published: "9/28/2012" 
+author: Kapil
+---
+
+Hello World.
