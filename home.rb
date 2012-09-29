3d42c329ae96cd66a943cd2b04693d14ff8553be
Kapil <kapil.kale.07@gmail.com>
2012-09-28 18:08:39 -0700

    blog added to site

--- home.rb
+++ home.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
 require 'haml'
 
+get '/blog/:post/?' do
+  File.read(File.join('public/blog', "#{params[:post]}", "index.html"))
+end
+
 get '/' do
   haml :home
 end
 
-get '/blog' do
+get '/blog/?' do
   haml :blog
 end
